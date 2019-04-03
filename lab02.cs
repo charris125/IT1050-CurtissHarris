3b73cc012ea83cf68339458cefe3a84cc49213ed
charris125 <48929365+charris125@users.noreply.github.com>
2019-04-03 18:00:53 -0400

    Add files via upload

---
+++ lab02.cs
@@ -0,0 +1,13 @@
+﻿//figure 3.17 Welcome4.cs
+// Displaying multiple line of text with string formatting
+using System.;
+
+public class Welcome 4
+namespace ConsoleApp2
+// Main method begins execution of C# application
+    public static void Main(string[] 
+        static void Main(string[] args)
+        Console.writeline{"{0}\{1}", "Hello World! from Curtiss", "C# prog
+        }
+    }// end Main
+} //end class Welcome 4
